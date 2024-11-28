6af6b247487d2218d2d434fc4fbd6b1678c4acef
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2024-11-28 14:01:30 +0100

    Created basic run-time triangle switching
    - Created a VAOs which I do the shape switching with

--- OpenGLExploring/OpenGLExploring/HelloTriangle/main.cpp
+++ OpenGLExploring/OpenGLExploring/HelloTriangle/main.cpp
@@ -54,6 +54,9 @@ static unsigned int createShader( const std::string& vertexShader, const std::st
 
 	// For following code, check the docs for more details:
 	// 
+	// When linking the shaders into a program, it links the output of each shader to the inputs of the next shader.
+	// This is also where you'll get LINKING ERRORS if your outputs and inputs do not match!!!
+	// 
 	// Attaches shaders to the program
 	glAttachShader( program, vs );
 	glAttachShader( program, fs );
@@ -75,6 +78,9 @@ int main( void )
 	/* Initialize the library */
 	if ( !glfwInit() )
 		return -1;
+	/*glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );*/
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
@@ -100,14 +106,17 @@ int main( void )
 		//	  x		 y
 			-0.5f, -0.5f, // Vertex 01
 			 0.5f, -0.5f, // Vertex 02
-			 0.5f,  0.5f,  // Vertex 03
-			-0.5f,  0.5f  // Vertex 04
+			 0.0f,  0.5f, // Vertex 03
+			//-0.5f,  0.5f  // Vertex 04
 	};
 
-	unsigned int indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
+
+	// Instruct OpenGL to generate 1 VAO
+	unsigned int vao1;
+	glGenVertexArrays( 1, &vao1 );
+	// then binds it so that for the next VBO, it register the buffer's attributes so that when switching from things to draw,
+	// you would not have to redefine the attributes for each shape/object but only bind its VAO.
+	glBindVertexArray( vao1 );
 
 	// Insruct OpenGL to generate 1 buffer in the VRAM
 	// and then set the location of that buffer to our local variable which actually represents a pointer to that buffer
@@ -126,7 +135,7 @@ int main( void )
 	// USAGE: with this we give a hint to OpenGL how to deal with that data:
 	//		  Providing GL_STATIC_DRAW instructs that we have the intention of setting the data only once and then only reading it;
 	//		  However, that does not mean that if we should modify it later on, we are forbidden to do so; no - we can, but it may be slower due to the way we have instructed OpenGL to interpret that data initially
-	glBufferData( GL_ARRAY_BUFFER, 8 * sizeof( float ), positions, GL_STATIC_DRAW );
+	glBufferData( GL_ARRAY_BUFFER, 6 * sizeof( float ), positions, GL_STATIC_DRAW );
 
 	// Instruct OpenGL to define an attribute from a vertex from the currently bound buffer so that we can later utilize it in our shader
 	// A Vertex is comprised of Attributes. An example of attributes could be 'Position', 'Color', 'Normal', etc.
@@ -141,11 +150,49 @@ int main( void )
 	glEnableVertexAttribArray( 0 );
 
 
+	// Create a second triangle so that in run-time I can swith between the two
+
+	float positions2[] = {
+		//	  x		 y
+			 0.0f, -0.5f, // Vertex 01
+			 0.5f,  0.5f, // Vertex 02
+			-0.5f,  0.5f, // Vertex 03
+			//-0.5f,  0.1f  // Vertex 04
+	};
+
+	unsigned int vao2;
+	glGenVertexArrays( 1, &vao2 );
+	glBindVertexArray( vao2 );
+
+	unsigned int buff2;
+	glGenBuffers( 1, &buff2 );
+	glBindBuffer( GL_ARRAY_BUFFER, buff2 );
+	glBufferData( GL_ARRAY_BUFFER, 6 * sizeof( float ), positions2, GL_STATIC_DRAW );
+
+	glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof( float ), ( void* ) 0 );
+	glEnableVertexAttribArray( 0 );
+
+
 	// Index buffer:
+	// Both triangles utilize the same index buffer
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		/*2, 3, 0*/
+	};
+
+
 	unsigned int ibo;
 	glGenBuffers( 1, &ibo );
 	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, ibo );
-	glBufferData( GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof( unsigned int ), indices, GL_STATIC_DRAW );
+	glBufferData( GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof( unsigned int ), indices, GL_STATIC_DRAW );
+
+
+	// switch to first VAO to set it up properly with the index buffer (like the second one above)
+	// to prevent undefined behavior
+	glBindVertexArray( vao1 );
+	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, ibo );
+
 
 
 	// Define shaders (BAD way)
@@ -173,20 +220,37 @@ void main(){
 	// BIND(select) the shader program for OpenGL to use
 	glUseProgram( shader );
 
+
+	bool shouldDisplayFirst = true;
+
+	int framesToShow = 30;
+	int counter = 1;
 	/* Loop until the user closes the window */
 	while ( !glfwWindowShouldClose( window ) )
 	{
 		/* Render here */
 		glClear( GL_COLOR_BUFFER_BIT );
 
-		glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr );
-		glDrawArrays( GL_TRIANGLES, 0, 3 );
+		if ( counter > framesToShow )
+		{
+			// Swap shapes
+			glBindVertexArray( shouldDisplayFirst ? vao2 : vao1 );
+
+			// Usually you would also swap index buffers but since we use the same one for both triangles it is not necessary
+
+			shouldDisplayFirst = !shouldDisplayFirst;
+			counter = 0;
+		}
+		glDrawElements( GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr );
+		//glDrawArrays( GL_TRIANGLES, 0, 3 );
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers( window );
 
 		/* Poll for and process events */
 		glfwPollEvents();
+
+		counter++;
 	}
 
 	// Free memory - opposite of glCreateProgram()
